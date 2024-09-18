0aec8375919943fbda4f017c109044d523a4f1b8
Huu Phuc Nguyen <huuphucmusic2306@gmail.com>
2024-09-17 23:37:35 -0500

    Time: 1 ms (60.76%) | Memory: 40.7 MB (13.31%) - LeetSync

--- 397-integer-replacement/integer-replacement.java
+++ 397-integer-replacement/integer-replacement.java
@@ -1,13 +1,22 @@
 class Solution {
 
+    private Map<Long, Integer> memo = new HashMap<>();
+
     private int integerReplacement(long n){
         if (n == 1) return 0;
 
+        if (memo.containsKey(n))
+            return memo.get(n);
+
+        int result;
         if (n % 2 == 0) {
-            return 1 + integerReplacement(n / 2);
+            result = 1 + integerReplacement(n / 2);
         } else {
-            return 1 + Math.min(integerReplacement(n + 1), integerReplacement(n - 1));
+            result = 1 + Math.min(integerReplacement(n + 1), integerReplacement(n - 1));
         }
+
+        memo.put(n, result);
+        return result;
     }
 
     public int integerReplacement(int n) {
