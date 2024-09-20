69f8b4a1a91912736418348ece3afcd2e1aedb32
Huu Phuc Nguyen <huuphucmusic2306@gmail.com>
2024-09-19 19:54:11 -0500

    Time: 161 ms (6.62%) | Memory: 11.6 MB (84.77%) - LeetSync

--- 397-integer-replacement/integer-replacement.py
+++ 397-integer-replacement/integer-replacement.py
@@ -1,23 +1,20 @@
 class Solution(object):
 
-    memo = {}
-
     def integerReplacement(self, n):
         """
         :type n: int
         :rtype: int
         """
 
-        if n in self.memo:
-            return self.memo[n]
-
-        if (n == 1):
+        if n == 1:
             return 0
-        elif (n%2 == 0):
-            result = 1 + self.integerReplacement(n/2)
+        elif n % 2 == 0:
+            return self.integerReplacement(n/2) + 1
         else:
-            result = 1+  min(self.integerReplacement(n+1), self.integerReplacement(n-1))
-        
-        self.memo[n] = result
-        return result
+            plus_1 = self.integerReplacement(n+1)
+            minus_1 = self.integerReplacement(n-1)
+            
+            result = min(plus_1, minus_1)
+            
+            return result + 1
         
