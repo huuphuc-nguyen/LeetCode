60af418c437119c58b476028861976860adb2e6c
Huu Phuc Nguyen <huuphucmusic2306@gmail.com>
2024-09-28 15:37:40 -0500

    Time: 23 ms (70.54%) | Memory: 11.5 MB (94.42%) - LeetSync

---
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def arithmeticTriplets(self, nums, diff):
+        """
+        :type nums: List[int]
+        :type diff: int
+        :rtype: int
+        """
+        result = 0
+
+        for i in range(0,len(nums)-1,1): 
+            number_next = nums[i] + diff 
+            number_next_next = nums[i] + diff + diff
+
+            if (number_next in nums) and (number_next_next in nums):
+                result += 1
+                print(nums[i], number_next, number_next_next)
+
+        return result
