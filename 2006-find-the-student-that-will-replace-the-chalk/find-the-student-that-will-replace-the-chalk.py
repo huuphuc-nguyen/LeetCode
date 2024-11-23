3dcb4ca52be09f49c4c11d3f3731b925816fa0f0
Huu Phuc Nguyen <huuphucmusic2306@gmail.com>
2024-11-23 12:51:22 -0600

    Time: 3 ms (86.61%) | Memory: 27.8 MB (85.56%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.py
@@ -1,22 +1,15 @@
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
-        
-
-        # n student
-        # n size chalk | sum(chalk) = k, if sum (chalk) < chalk[i]
-        # student i use chalk i
-
-
-        # use k - chalk[i] => if result is negative => return i
 
         chalk_for_one_round = sum(chalk)
 
         # cfor > given k O(n)
         # cfor == given k O(n)
         # cfor < given k
-        if chalk_for_one_round < k:
-            k = k%chalk_for_one_round
+        k = k%chalk_for_one_round
 
+        # Idea: decrease the k until it is negative => probelm: if k is to gib have to run so many round
+        # Solve: calculate the sum of array then use modular
         i = 0
 
         while True:
