2710be1f64485435331847fa35563236441954a3
Huu Phuc Nguyen <huuphucmusic2306@gmail.com>
2024-12-14 21:36:26 -0600

    Time: 27 ms (48.75%) | Memory: 22.8 MB (48.30%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -3,16 +3,32 @@ class Solution:
         #   1         2           3          4 
     #result: 2 3 4     1 3 4       1 2 4      1 2 3 
 
-    #preifx:  1           1          1 2       1 2 3
+    #preifx:  1           1         1 2       1 2 3
     #suffix: 1 2 3 4     3 4        4          1
 
+        # prefix= [0] * len(nums)
+        # suffix= [0] * len(nums)
+        # prefix[0] = 1
+        # suffix[len(nums)-1]=1
+        # for i in range(1, len(nums)):
+        #     prefix[i] = nums[i-1] * prefix[i-1]
+        # for i in range(len(nums)-2,-1,-1):
+        #     suffix[i] = nums[i+1] * suffix[i+1]
+
+        # return [a * b for a, b in zip(prefix, suffix)]
+
+        # Followup: utilize prefix as the return array, use 1 variable to keep track suffix sum
+        
         prefix= [0] * len(nums)
-        suffix= [0] * len(nums)
         prefix[0] = 1
-        suffix[len(nums)-1]=1
         for i in range(1, len(nums)):
             prefix[i] = nums[i-1] * prefix[i-1]
-        for i in range(len(nums)-2,-1,-1):
-            suffix[i] = nums[i+1] * suffix[i+1]
+        
+        suffix = 1
+        
+        for i in range(len(nums)-1,-1,-1):
+            prefix[i] *= suffix
+            suffix *= nums[i]
+        
+        return prefix
 
-        return [a * b for a, b in zip(prefix, suffix)]
