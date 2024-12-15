69f1fb7d89187eec1bf9d28a017ce445d77a1437
Huu Phuc Nguyen <huuphucmusic2306@gmail.com>
2024-12-14 21:40:36 -0600

    Time: 23 ms (64.13%) | Memory: 22.7 MB (57.90%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -17,15 +17,17 @@ class Solution:
 
         # return [a * b for a, b in zip(prefix, suffix)]
 
-        # Followup: utilize prefix as the return array, use 1 variable to keep track suffix sum
+        # Followup: utilize prefix as the return array, use 1 variable to keep track suffix product after each iteration
         
-        prefix= [0] * len(nums)
-        prefix[0] = 1
+        prefix= [1] * len(nums)
+        
+        # create prefix product array
         for i in range(1, len(nums)):
             prefix[i] = nums[i-1] * prefix[i-1]
         
         suffix = 1
         
+        # iterate backwards to accumulate the suffix product
         for i in range(len(nums)-1,-1,-1):
             prefix[i] *= suffix
             suffix *= nums[i]
