f1319410f9dd5fbcfa2c670941c35631e1d87977
Huu Phuc Nguyen <huuphucmusic2306@gmail.com>
2025-01-06 15:56:06 -0600

    Time: 89 ms (29.14%) | Memory: 41.6 MB (48.13%) - LeetSync

--- 2130-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
+++ 2130-maximum-twin-sum-of-a-linked-list/maximum-twin-sum-of-a-linked-list.py
@@ -5,15 +5,44 @@
 #         self.next = next
 class Solution:
     def pairSum(self, head: Optional[ListNode]) -> int:
-        arr = []
-        current = head
-        while current:
-            arr.append(current.val)
-            current = current.next
+        #arr = []
+        # current = head
+        # while current:
+        #     arr.append(current.val)
+        #     current = current.next
         
-        maxi = 0
-        for i in range(len(arr)//2):
-            sum = arr[i] + arr[len(arr)-1-i]
-            maxi = max(maxi, sum)
+        # maxi = 0
+        # for i in range(len(arr)//2):
+        #     sum = arr[i] + arr[len(arr)-1-i]
+        #     maxi = max(maxi, sum)
 
+        # return maxi
+
+        # find the middle node
+        fast = slow = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # reverse the 2nd half
+        sec_head = slow
+        prev = None
+        current = sec_head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        
+        # reverse through 2 list
+        c1 = head
+        c2 = prev
+        maxi = 0
+        while c1 and c2:
+            maxi = max(maxi, c1.val + c2.val)
+            c1=c1.next
+            c2=c2.next
+        
         return maxi
+
