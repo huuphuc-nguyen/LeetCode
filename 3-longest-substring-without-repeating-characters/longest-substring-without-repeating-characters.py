33c7ce095125c0a8eaa5b90ee97d8e889a6bdbb3
Huu Phuc Nguyen <huuphucmusic2306@gmail.com>
2025-06-25 00:53:51 -0500

    Time: 47 ms (15.34%) | Memory: 18 MB (34.04%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,24 +1,27 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        char_set = set()
-        
-        start = 0
-        max_length = 0
-        
-        for end in range(len(s)):
-            if s[end] in char_set:
-                while s[end] in char_set:
-                    char_set.remove(s[start])
-                    start += 1
-            
-            char_set.add(s[end])
-            
-            current_length = end - start + 1
-            if current_length > max_length:
-                max_length = current_length
-        
-        return max_length
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+         # pw*wk*ew
+        # max = 2
+        if s is None or s == "": return 0
+
+        left = right = 0
+        max_lenght = 1
+        window = []
+        window.append(s[0])
+
+        for i in range(1,len(s)):
+            # check the next char to see if it is in the window or not
+            if s[i] not in window:
+                window.append(s[i])
+                continue
+            else:
+                print(max_lenght)
+            # update maxlenght
+                if len(window) > max_lenght:
+                    max_lenght = len(window)
+            # narrow the window until the repeated character is not in the window anymore
+                while len(window) != 0 and s[i] in window :
+                    window.pop(0)
+                window.append(s[i])
+        print(window)
+        return max(max_lenght, len(window))
