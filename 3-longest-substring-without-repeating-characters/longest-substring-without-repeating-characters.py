8e3a5586b57301f2e0f3ab081badce8752aa09fa
Huu Phuc Nguyen <huuphucmusic2306@gmail.com>
2025-06-25 01:00:49 -0500

    Time: 24 ms (25.44%) | Memory: 18.1 MB (12.14%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -2,26 +2,38 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
          # pw*wk*ew
         # max = 2
-        if s is None or s == "": return 0
+        # if s is None or s == "": return 0
 
-        left = right = 0
-        max_lenght = 1
-        window = []
-        window.append(s[0])
+        # left = right = 0
+        # max_lenght = 1
+        # window = []
+        # window.append(s[0])
 
-        for i in range(1,len(s)):
-            # check the next char to see if it is in the window or not
-            if s[i] not in window:
-                window.append(s[i])
-                continue
-            else:
-                print(max_lenght)
-            # update maxlenght
-                if len(window) > max_lenght:
-                    max_lenght = len(window)
-            # narrow the window until the repeated character is not in the window anymore
-                while len(window) != 0 and s[i] in window :
-                    window.pop(0)
-                window.append(s[i])
-        print(window)
-        return max(max_lenght, len(window))
+        # for i in range(1,len(s)):
+        #     # check the next char to see if it is in the window or not
+        #     if s[i] not in window:
+        #         window.append(s[i])
+        #         continue
+        #     else:
+        #         print(max_lenght)
+        #     # update maxlenght
+        #         if len(window) > max_lenght:
+        #             max_lenght = len(window)
+        #     # narrow the window until the repeated character is not in the window anymore
+        #         while len(window) != 0 and s[i] in window :
+        #             window.pop(0)
+        #         window.append(s[i])
+        # print(window)
+        # return max(max_lenght, len(window))
+
+        wd = set()
+        left = 0
+        maxl = 0
+
+        for right in range(len(s)):
+            while s[right] in wd:
+                wd.remove(s[left])
+                left += 1
+            wd.add(s[right])
+            maxl = max(maxl, right - left + 1)
+        return maxl
