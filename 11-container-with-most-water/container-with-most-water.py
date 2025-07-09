e1a68605fea58e98d38b44a2fa312392e8812576
Huu Phuc Nguyen <huuphucmusic2306@gmail.com>
2025-07-09 10:48:03 -0500

    Time: 70 ms (89.68%) | Memory: 28.6 MB (23.73%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,26 +1,12 @@
-class Solution(object):
-    def maxArea(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        # Create largest range, by creating a left and right pointer
-        l, r = 0, len(height) - 1
-        maxArea = 0
-
-        # At every part of the range calculate the current area at this width and check against our max area
-        while l < r:
-            width = r - l
-            minHeight = min(height[l], height[r])
-            currArea = minHeight * width
-            maxArea = max(maxArea, currArea)
-
-            # Reduce the range/width while maintaining the maximum height 
-            if height[l] < height[r]:
-                l += 1
-            else:
-                r -= 1
-
-        # Return max area found
-        return maxArea
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        result = 0
+        while left < right:
+            area = min(height[left],height[right]) * (right - left)
+            if area > result: result = area
+            if height[left] < height[right]: left += 1
+            else: right -=1
+        return result
         
