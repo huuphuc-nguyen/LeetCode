fcae4ad9d8aac998cb3c0ede25e282645a5acdbd
Huu Phuc Nguyen <huuphucmusic2306@gmail.com>
2025-07-16 01:53:59 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (81.38%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = []
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+            for num in nums:
+                if num not in path:
+                    path.append(num)
+                    backtrack(path)
+                    path.pop()
+
+        backtrack([])
+        return result
